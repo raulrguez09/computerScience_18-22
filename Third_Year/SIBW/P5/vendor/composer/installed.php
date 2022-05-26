bbc8ef6095a8e46b7c5b9a2c5b2a0e5eddd5f1b2
raulrguez09 <raulbarsa09@gmail.com>
2022-05-27 01:57:50 +0200

    sibw 3

---
+++ Third_Year/SIBW/P5/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '7.4.x-dev',
+    'version' => '7.4.9999999.9999999-dev',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '24953449b8014b4dbc24e8efbb42d0e868a25f31',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '7.4.x-dev',
+      'version' => '7.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '24953449b8014b4dbc24e8efbb42d0e868a25f31',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1f3b7e2c06cc05d42936a8ad508ff1db7975cdc5',
+    ),
+  ),
+);
